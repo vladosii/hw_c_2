9d4c4b1e075d8cecb15925569920f2e8e2fa763b
Vlad Tsapok <vlad.capok@mail.ru>
2023-03-01 15:08:33 +0300

    решение первой задачи

--- hw_c_2_1/hw_2_1.cs
+++ hw_c_2_1/hw_2_1.cs
@@ -1,3 +1,15 @@
 //Задача 10: Напишите программу, которая принимает 
 //на вход трёхзначное число и на выходе показывает 
 //вторую цифру этого числа.
+
+using System;
+
+string str_new = Console.ReadLine()!;
+
+int len = str_new.Length;
+
+if (len < 3){
+    Console.WriteLine("В строке меньше 3 цифр!");
+}else{
+    Console.WriteLine($"Второй элемент строки: {str_new[1]}");
+}
